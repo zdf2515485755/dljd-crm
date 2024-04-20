fcc637625a2b01b0d72c4f45210476fcfc40edee
mrzhang <2515485755@qq.com>
2024-04-20 18:50:51 +0800

    fix:Building-a-code-framework

---
+++ dljd-master/dljd-web/src/main/java/com/zdf/dljdweb/DljdWebApplication.java
@@ -0,0 +1,16 @@
+package com.zdf.dljdweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mrzhang
+ */
+@SpringBootApplication
+public class DljdWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DljdWebApplication.class, args);
+    }
+
+}
