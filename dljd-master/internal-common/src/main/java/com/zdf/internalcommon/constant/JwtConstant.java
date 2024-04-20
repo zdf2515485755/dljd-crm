4543a40eea1ce8ef7eef0c671ae8b301431e67ec
mrzhang <2515485755@qq.com>
2024-04-20 19:39:30 +0800

    feature:Add-basic-configuration-file

---
+++ dljd-master/internal-common/src/main/java/com/zdf/internalcommon/constant/JwtConstant.java
@@ -0,0 +1,12 @@
+package com.zdf.internalcommon.constant;
+
+/**
+ *@Description Jwt constant
+ *@Author mrzhang
+ *@Date 2024/3/25 18:36
+ */
+public class JwtConstant {
+    public static final String SIGN = "sass1524&'%$";
+
+    private JwtConstant(){}
+}
